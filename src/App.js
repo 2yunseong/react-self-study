fa1b67e91cf3ce9df9169d123440e23f08653c62
2yunseong <dbsdltjd3701@naver.com>
2022-02-08 23:52:23 +0900

    feat : add counter example to Class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Counter from "./chap03/Counter";
 import MyChildren from "./chap03/MyChildren";
 import MyComponent from "./chap03/MyComponent.js";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <MyChildren name="yunseong">Test</MyChildren>
       <MyComponent name="yunseong2">Test</MyComponent>
+      <Counter />
     </div>
   );
 }

23da884abab3ef64bbfd7d50ef0b01438ce66e1c
2yunseong <dbsdltjd3701@naver.com>
2022-02-09 00:35:39 +0900

    feat : 입/퇴장 로직 개선

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Counter from "./chap03/Counter";
 import MyChildren from "./chap03/MyChildren";
 import MyComponent from "./chap03/MyComponent.js";
+import Say from "./chap03/Say";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <MyChildren name="yunseong">Test</MyChildren>
       <MyComponent name="yunseong2">Test</MyComponent>
       <Counter />
+      <Say />
     </div>
   );
 }

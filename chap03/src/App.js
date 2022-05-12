f1252054fc0d868378f2b7311db2e98f4ed8f508
2yunseong <dbsdltjd3701@naver.com>
2022-05-13 02:33:27 +0900

    feat : chap03 props 예제 실습

---
+++ chap03/src/App.js
@@ -0,0 +1,15 @@
+import ChildComponent from './ChildComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      
+        <ChildComponent>Children</ChildComponent>          
+    </div>
+  );
+}
+
+export default App;
---
+++ chap03/src/ChildComponent.js
@@ -0,0 +1,12 @@
+const ChildComponent = props => {
+    return <div>
+        hello, I' m{props.name} and I'm {props.children}
+    </div>
+}
+
+ChildComponent.defaultProps = {
+    name: "yunseong"
+};
+
+
+export default ChildComponent;

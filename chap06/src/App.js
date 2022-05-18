502ccb94a284fe4689d73f94908d27a7e4c99aeb
2yunseong <dbsdltjd3701@naver.com>
2022-05-18 20:23:05 +0900

    feat : chap06 실습 ~ 데이터 추가

---
+++ chap06/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import IterationExample from './IterationExample';
+
+function App() {
+  return (
+    <div className="App">
+      <IterationExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ chap06/src/IterationExample.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+const IterationExample = () => {
+    const [users, setUsers] = useState([
+        {id : 1 , name : "yunseong"},
+        {id : 2 , name : "yunseong2"},
+        {id : 3 , name : "yunseong3"},
+    ]); 
+    
+    const [usersId, setUsersId] = useState(4);
+    const [inputName, setInputName] = useState("");
+
+    const onChange = (e) => {
+        setInputName(e.target.value);
+    }
+
+    const onClick = () => {
+        setUsers([
+            ...users,
+            {id : usersId, name : inputName}
+        ]);
+        setUsersId(usersId + 1);
+        setInputName("");
+    }
+    
+    const onDelete = () => {
+
+    }
+
+    return(<div>
+        <input type="text" placeholder="write name.." onChange={onChange} value={inputName}/>
+        <button onClick={onClick}>인원 추가</button>
+        <button onClick={onDelete}>인원 삭제</button>
+        {users.map((user) => {
+            return <div key={user.id}>{user.name}</div>
+        })}
+    </div>);
+    
+}
+
+export default IterationExample;

9860d28e9dbb437e588fd8380b2744206b730418
2yunseong <dbsdltjd3701@naver.com>
2022-05-26 19:25:25 +0900

    feat(About): useSearchParams 이용한 쿼리스트링 파싱

--- rrd_example/src/About.js
+++ rrd_example/src/About.js
@@ -1,17 +1,29 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const About = () => {
-    const location = useLocation();
-    console.dir(location);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const detail = searchParams.get('detail');
+    const mode = searchParams.get('mode');
 
+    const onToggleDetail = () => {
+        setSearchParams({mode, detail: detail === 'true' ? false : true});
+    }
+    
+    const onIncreaseMode = () => {
+        const nextMode = null ? 1 : parseInt(mode) + 1;
+        setSearchParams({mode: nextMode , detail});
+    }
     return(
         <div>
             <h1>About</h1>
             <p>예제 화면</p>
-            <p>쿼리스트링:{location.search}</p>
+            <p>detail:{detail}</p>
+            <button onClick={onToggleDetail}>토글 변경</button>
+            <p>mode:{mode}</p>
+            <button onClick={onIncreaseMode}>모드 1 증가</button> <br />
             <Link to="/">홈으로</Link>
         </div>
-    )
+    );
 }
 
 export default About;

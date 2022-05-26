1f41d353568417cd07ef802070c9b8555127491b
2yunseong <dbsdltjd3701@naver.com>
2022-05-26 20:35:03 +0900

    feat(Articles) : 중첩 라우트 기능을 통한 Article 구현

--- rrd_example/src/App.js
+++ rrd_example/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './Home';
 import About from './About';
 import Profile from './Profile';
+import Article from './Article';
+import Articles from './Articles';
 
 import { Route, Routes } from "react-router-dom";
 
@@ -11,6 +13,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About/>} />
       <Route path="/profile/:username/" element={<Profile />} />
+      <Route path="/articles" element={<Articles />} >
+        <Route path=":id" element={<Article />} />
+      </Route>
+      
     </Routes>
   );
 }

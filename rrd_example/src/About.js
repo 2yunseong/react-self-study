484ae8796351a343173398ba9ed5f6f85263568b
2yunseong <dbsdltjd3701@naver.com>
2022-05-26 19:04:22 +0900

    feat(About) : 쿼리스트링 예제 useLocation 사용

--- rrd_example/src/About.js
+++ rrd_example/src/About.js
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const About = () => {
+    const location = useLocation();
+    console.dir(location);
+
     return(
         <div>
             <h1>About</h1>
             <p>예제 화면</p>
+            <p>쿼리스트링:{location.search}</p>
             <Link to="/">홈으로</Link>
         </div>
     )

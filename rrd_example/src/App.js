e9622654813cccfeccedb11d3edb1a610dcb1b95
2yunseong <dbsdltjd3701@naver.com>
2022-05-26 22:41:39 +0900

    feat(NotFound) : Route path 와일드카드 이용한 NotFound 라우팅

--- rrd_example/src/App.js
+++ rrd_example/src/App.js
@@ -4,19 +4,23 @@ import About from './About';
 import Profile from './Profile';
 import Article from './Article';
 import Articles from './Articles';
+import Layout from './Layout';
+import NotFound from './NotFound';
 
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/profile/:username/" element={<Profile />} />
+      <Route element={<Layout />}> 
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/profile/:username/" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />} >
         <Route path=":id" element={<Article />} />
       </Route>
-      
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }

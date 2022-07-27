45e4aa5f4870310bdd61d8cee358470afd331223
yunseong76039287 <dbsdltjd3701@naver.com>
2022-07-28 03:36:11 +0900

    feat: color 선택 로직 추가

--- chap15/src/App.js
+++ chap15/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ColorBox from './components/ColorBox.js';
+import SelectColors from './components/SelectColors';
 import { ColorProvider } from './contexts/color.js';
 
 function App() {
   return (
       <ColorProvider>
         <div className="App">
+          <SelectColors />
           <ColorBox />
         </div>
       </ColorProvider>    
--- chap15/src/components/ColorBox.js
+++ chap15/src/components/ColorBox.js
@@ -5,11 +5,20 @@ const ColorBox = () => {
         <ColorContext.Consumer>
         {
             ({ state }) => (
+                <>
                 <div style={{
                     width: '64px',
                     height: '64px',
                     background: state.color
                 }} />
+                <div 
+                    style={{
+                        width: '32px',
+                        height: '32px',
+                        background: state.subcolor                        
+                    }}
+                />
+                </>
             )
         }
         </ColorContext.Consumer>

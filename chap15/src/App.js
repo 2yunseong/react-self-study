6b38ec13a6d058d769cd074c04e5f5b537405a62
yunseong76039287 <dbsdltjd3701@naver.com>
2022-07-28 01:24:53 +0900

    feat: chap15 초기화

---
+++ chap15/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ColorBox from './components/ColorBox.js';
+function App() {
+  return (
+    <div className="App">
+      <ColorBox />
+    </div>
+  );
+}
+
+export default App;

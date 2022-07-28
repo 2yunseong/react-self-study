d9275bb5f65c40b4423facc7596851576abe8c4b
2yunseong <dbsdltjd3701@naver.com>
2022-07-28 15:01:07 +0900

    feat: useContext 사용 - ColorBox 에서 컨슈머 사용을 useContext Hooks 사용으로 변경

--- chap15/src/components/ColorBox.js
+++ chap15/src/components/ColorBox.js
@@ -1,28 +1,28 @@
+import { useContext } from "react";
 import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
-    return(
-        <ColorContext.Consumer>
-        {
-            ({ state }) => (
-                <>
-                <div style={{
-                    width: '64px',
-                    height: '64px',
-                    background: state.color
-                }} />
-                <div 
-                    style={{
-                        width: '32px',
-                        height: '32px',
-                        background: state.subcolor                        
-                    }}
-                />
-                </>
-            )
-        }
-        </ColorContext.Consumer>
-    );
-}
+  const { state } = useContext(ColorContext); // consumer 대신 useContext 사용하여 비구조화 할당으로 선언
+
+  // 바로 Context를 사용할 수 있다.
+  return (
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
+  );
+};
 
 export default ColorBox;

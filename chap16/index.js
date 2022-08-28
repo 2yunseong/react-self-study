ce2fe2d7f670b0f8e4c59a20dfcbd92b15739b9f
2yunseong <dbsdltjd3701@naver.com>
2022-08-28 20:22:36 +0900

    feat: reducer 함수 생성

--- chap16/index.js
+++ chap16/index.js
@@ -15,3 +15,16 @@ const InitialState = {
   toggle: false,
   counter: 0,
 };
+
+function reducer(state = InitialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return { ...state, toggle: !state.toggle };
+    case INCREASE:
+      return { ...state, counter: state.counter + 1 };
+    case DECREASE:
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+}

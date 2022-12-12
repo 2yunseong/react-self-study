32dfcb8fdacb52f7dc5fa1df7009f549b88db7df
2yunseong <dbsdltjd3701@naver.com>
2022-12-12 23:59:08 +0900

    feat(chap09): css module 실습

--- chap09/src/App.js
+++ chap09/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CSSModule from './CSSModule/CSSModule';
 import SassComponent from './SassComponent';
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
+      <CSSModule />
     </div>
   );
 }

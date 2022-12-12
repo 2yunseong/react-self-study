efc4503859934ea07606f0eeb2e00c001a234a76
2yunseong <dbsdltjd3701@naver.com>
2022-12-13 03:55:21 +0900

    feat(chap09): styled component 실습

--- chap09/src/App.js
+++ chap09/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import CSSModule from './CSSModule/CSSModule';
 import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
       <CSSModule />
+      <StyledComponent />
     </div>
   );
 }

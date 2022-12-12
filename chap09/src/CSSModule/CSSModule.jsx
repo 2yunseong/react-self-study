7e0fe8779178e5d3480a3384ca57f48c6f6fe0d8
2yunseong <dbsdltjd3701@naver.com>
2022-12-13 00:20:54 +0900

    refactor(chap09): module css sass에 적용

--- chap09/src/CSSModule/CSSModule.jsx
+++ chap09/src/CSSModule/CSSModule.jsx
@@ -1,10 +1,10 @@
 import classNames from 'classnames/bind';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 
 const c = classNames.bind(styles);
 
 const CSSModule = () => {
-    return <div className={c('wrapper')}>
+    return <div className={c('wrapper', 'inverted')}>
         안녕하세요, 저는 <span className='something'>CSS Module</span> 입니다!
     </div>
 }

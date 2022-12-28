9f7e77135ac285db61a3abb97d82c4662ff02e74
2yunseong <dbsdltjd3701@naver.com>
2022-12-28 20:56:43 +0900

    feat(chap14): 뉴스 불러오기 로직 구현

--- chap14/src/App.js
+++ chap14/src/App.js
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { API_KEY } from './env/env';
+
 function App() {
-  return <div className='App'></div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className='App'>
+      <button onClick={onClick}>불러오기</button>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;

2f9ce770398eb5b8a9c54ec5094093624120942e
2yunseong <dbsdltjd3701@naver.com>
2022-12-31 21:22:57 +0900

    feat(chap15): useContext를 사용한 Consumer 사용 대체

--- chap15/src/contexts/color.js
+++ chap15/src/contexts/color.js
@@ -1,29 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
 
 // createContext에 기본 값 전달
-const ColorContext = createContext({ 
-    state: { color: 'black', subcolor: 'red'},
-    actions: {
-        setColor: () => {},
-        setSubcolor: () => {}
-    }
+const ColorContext = createContext({
+  state: { color: 'black', subcolor: 'red' },
+  actions: {
+    setColor: () => {},
+    setSubcolor: () => {},
+  },
 });
 
-// Provider 컴포넌트 
+// Provider 컴포넌트
 const ColorProvider = ({ children }) => {
-    const [color, setColor] = useState('black');
-    const [subcolor, setSubcolor] = useState('red');
+  const [color, setColor] = useState('black');
+  const [subcolor, setSubcolor] = useState('red');
 
-    // createConext의 기본 값은 실제 Provider의 value에 넣는 객체의 형태와 일치시켜 주는 것이 좋다.
-    const value = {
-        state: {color, subcolor},
-        actions: {setColor, setSubcolor}
-    };
+  // createConext의 기본 값은 실제 Provider의 value에 넣는 객체의 형태와 일치시켜 주는 것이 좋다.
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor },
+  };
 
-    return (
-        <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
-    );
-}
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
 
 // ColorContext 객체 안에 있는 Consumer를 ColorConsumer로 선언하겠다와 같은 의미이다.
 const { Consumer: ColorConsumer } = ColorContext;

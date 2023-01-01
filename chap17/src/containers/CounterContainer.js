7909f29596d3c614555316d358ac478ff9308b4a
2yunseong <dbsdltjd3701@naver.com>
2023-01-02 02:54:33 +0900

    feat(chap17): counter container 재구현

--- chap17/src/containers/CounterContainer.js
+++ chap17/src/containers/CounterContainer.js
@@ -1,16 +1,22 @@
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import Counter from "../components/Counter";
-import { decrease, increase } from "../modules/counter";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
   return <Counter number={number} increase={increase} decrease={decrease} />;
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
+const mapStateToProps = ({ counter }) => ({
+  number: counter.number,
 });
 
-export default connect(mapStateToProps, { increase, decrease })(
-  CounterContainer
-);
+export default connect(mapStateToProps, (dispatch) =>
+  bindActionCreators({ increase, decrease }, dispatch)
+)(CounterContainer);
+
+/*
+  혹은
+  export default connect(mapStateToProps, { increase, decrease })(CounterContainer);
+  이러면 connect가 내부적으로  dispatch 함수를 처리해준다.
+  */

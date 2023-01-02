b31166cb468e3841c5730ebac49d6e9f8f5f38fd
2yunseong <dbsdltjd3701@naver.com>
2023-01-03 02:18:23 +0900

    feat(chap18): redux-logger 적용

--- chap18/src/index.js
+++ chap18/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 
 import rootReducer from './modules';
 import App from './App';
-import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

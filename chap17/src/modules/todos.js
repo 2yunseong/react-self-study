79f011e10063e0bb055a74107a6b47afb1b7d835
2yunseong <dbsdltjd3701@naver.com>
2023-01-02 16:50:42 +0900

    refactor(chap17): immer 사용으로 가독성 개선

--- chap17/src/modules/todos.js
+++ chap17/src/modules/todos.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { createAction, handleActions } from 'redux-actions';
 
 // 액션 정의
@@ -27,30 +28,25 @@ const initialState = {
 
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({
-      ...state,
-      inputs: input,
-    }),
-    [INSERT]: ({ todos, inputs }, action) => ({
-      inputs: '',
-      todos: [...todos, { id: id++, text: inputs, done: false }],
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map((todo) => {
-        if (id === todo.id) {
-          return {
-            ...todo,
-            done: !todo.done,
-          };
-        }
-        return todo;
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.inputs = input;
       }),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter((todo) => todo.id !== id),
-    }),
+    [INSERT]: (state, action) =>
+      produce(state, (draft) => {
+        draft.todos.push({ id: id++, text: draft.inputs, done: false });
+        draft.inputs = '';
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
   },
   initialState
 );
